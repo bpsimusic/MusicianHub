cc4b417436f2d1f8d77273ba6cb1270c6b99b56c
bpsimusic <bpsimusic@gmail.com>
2016-12-10 16:43:19 -0800

    add play icon to each image

--- frontend/components/sample_list_item.jsx
+++ frontend/components/sample_list_item.jsx
@@ -16,6 +16,13 @@ class SampleListItem extends React.Component {
   render(){
       return (
         <li className={"sample-artist"}>
+          <div className={"play-button-image"}
+            onClick={this.playSong(this.props.artist.song, this.props.artist)}>
+            <span className="fa-stack fa-lg">
+              <i className="fa fa-circle fa-stack-2x"></i>
+              <i className="fa fa-play fa-stack-1x"></i>
+            </span>
+          </div>
           <img src={this.props.artist.image_url}
                className={"sample-artist-image"}
                onClick={this.playSong(this.props.artist.song, this.props.artist)}>
