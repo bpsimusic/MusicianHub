ae1209312de62527a316b425155f02af8fcfbac8
bpsimusic <bpsimusic@gmail.com>
2016-12-14 13:47:08 -0800

    remove minified error

---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+var webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/musicians.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+      test: /\.html$/,
+      loader: 'html-loader?attrs[]=video:src'
+    }, {
+      test: /\.mp4$/,
+      loader: 'url?limit=10000&mimetype=video/mp4'
+    }]
+  },
+  devtool: 'source-maps'
+};
